1ba753a996c1808a23f1c9fecf6241038b8ec6a7
Hammad214508 <hammadmuhammad15@gmail.com>
2020-09-21 19:15:11 +0100

    use db data for ML

--- MachineLearning/load_data.py
+++ MachineLearning/load_data.py
@@ -1,13 +1,31 @@
+import mysql.connector
 import pandas as pd
 from surprise import Dataset
 from surprise import Reader
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="my_database"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""SELECT USER_ID, FOOD_ID, RATING FROM RATINGS""")
+
+myresult = mycursor.fetchall()
+
+user_ids = [i[0] for i in myresult]
+food_ids = [i[1] for i in myresult]
+ratings = [i[2] for i in myresult]
+
 # This is the same data that was plotted for similarity earlier
 # with one new user "E" who has rated only movie 1
 ratings_dict = {
-    "item": [1, 2, 1, 2, 1, 2, 1, 2, 1],
-    "user": ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E'],
-    "rating": [1, 2, 2, 4, 2.5, 4, 4.5, 5, 3],
+    "user"  : user_ids,
+    "item"  : food_ids,
+    "rating": ratings,
 }
 
 df = pd.DataFrame(ratings_dict)
--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -2,5 +2,5 @@ from load_data import data
 from recommender import algo
 trainingSet = data.build_full_trainset()
 algo.fit(trainingSet)
-prediction = algo.predict('E', 2)
+prediction = algo.predict(3, 6)
 print(prediction.est)
